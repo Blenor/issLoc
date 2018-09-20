4937b1da5bcab3d863722dbd51440c3104b81153
artur <arturalberth@gmail.com>
2018-09-20 19:39:26 +0200

    test

--- venv/main.py
+++ venv/main.py
@@ -1,7 +1,7 @@
 import json
 import turtle
-import tkinter as tk
 import urllib.request
+import time
 
 url = 'http://api.open-notify.org/astros.json'
 response = urllib.request.urlopen(url)
@@ -26,6 +26,40 @@ print('Longitude: ', lon)
 
 screen = turtle.Screen()
 screen.setup(720, 360)
-#screen.setworldcoordinates(-180, -90, 180, 90)
+screen.setworldcoordinates(-180, -90, 180, 90)
 screen.bgpic('map.png')
 
+screen.register_shape('iss.gif')
+iss = turtle.Turtle()
+iss.shape('iss.gif')
+iss.setheading(90)
+
+iss.penup()
+iss.setpos((float(lon)),(float(lat)))
+
+#Krakow, Poland
+lat = 50.006367
+lon = 19.956079
+
+location = turtle.Turtle()
+location.penup()
+location.color('yellow')
+location.setpos((float(lon)),(float(lat)))
+location.dot(5)
+location.hideturtle()
+
+url = 'http://api.open-notify.org/iss-pass.json'
+url = url + '?lat=' + str(lat) + '&lon=' + str(lon)
+response = urllib.request.urlopen(url)
+result = json.loads(response.read())
+
+over = result['response'][1]['risetime']
+#print(over)
+
+style = ('Arial', 10, 'bold')
+location.write(time.ctime(over), font=style)
+
+
+
+
+turtle.mainloop()
--- venv/trutel.py
+++
@@ -1,5 +0,0 @@
-import turtle
-
-screen = turtle.Screen()
-screen.setup(600, 400)
-screen.bgpic('map.gif')
